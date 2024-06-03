43f9dca13921c2a0bffb00dc234d8a3d71f7a876
Natali258 <natalya.holyavko@gmail.com>
2024-06-03 17:18:02 +0300

    add responsive img

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,5 +1,18 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 export const HomePage = () => {
-  return <div>HomePage</div>;
+  return <div>
+    <div>
+      <h1>The road to the depths of the human soul</h1>
+      <p>We help you to reveal your potential, overcome challenges and find a guide in your own life with the help of our experienced psychologists.</p>
+      <NavLink>
+        <p>Get started</p>
+
+      </NavLink>
+    </div>
+    <div>
+      <img src="/src/images/foto/imagewomen.jpg" alt="woman" />
+    </div>
+  </div>;
 };
