1d6690a143724307e339de08fd8bbdd1a07eeb38
Natali258 <natalya.holyavko@gmail.com>
2024-06-16 22:11:50 +0300

    add HomePage-styled

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
 import { MainPhoto } from "../../components/MainPhoto/MainPhoto";
+import { Icon } from "../../components/Icon/Icon";
+import { SButton, SButtonIcon, SButtonText } from "./HomePage.styled";
 
 
 
@@ -12,9 +13,12 @@ export const HomePage = () => {
     <div>
       <h1>The road to the depths of the human soul</h1>
       <p>We help you to reveal your potential, overcome challenges and find a guide in your own life with the help of our experienced psychologists.</p>
-      <NavLink>
-        <p>Get started</p>
-      </NavLink>
+      <SButton>
+        <SButtonText>Get started</SButtonText>
+        <SButtonIcon>
+          <Icon name='arrow-up-right' size='18' fill='#fbfbfb'/>
+        </SButtonIcon>
+      </SButton>
     </div>
     <div>
       <MainPhoto/>
--- src/pages/HomePage/HomePage.styled.js
+++ src/pages/HomePage/HomePage.styled.js
@@ -1,6 +1,31 @@
+import { NavLink } from "react-router-dom";
 import styled from "styled-components";
 
-export const Spicture = styled.div`
-background-image: url('../../images/hero/women-desktop_@1x.jpg');
-  
+export const SButton = styled(NavLink)`
+/* display: flex;
+flex-direction: row;
+align-items: center;
+gap: 17px; */
+display: block;
+border-radius: 30px;
+padding: 18px 50px;
+width: 235px;
+height: 60px;
+background: #3470ff; 
+text-decoration: none;
+`
+
+export const SButtonText = styled.p`
+display: inline;
+font-weight: 500;
+font-size: 20px;
+line-height: 120%;
+letter-spacing: -0.01em;
+color: #fbfbfb;
+`
+
+export const SButtonIcon = styled.div`
+display: inline-block;
+margin-left: 17px;
+width: 18px;
 `
