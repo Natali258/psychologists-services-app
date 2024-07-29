1bccacc965f62f0545dd57e9736b5b48ced6549e
Natali258 <natalya.holyavko@gmail.com>
2024-07-29 13:02:12 +0300

    add Filter component

---
+++ src/components/Filter/Filter.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export const Filter = () => {
+  return (
+    <div>
+      <p>Filters</p>
+      <button>A to Z
+
+      </button>
+      <ul>
+        <li>A to Z</li>
+        <li>Z to A</li>
+        <li>Less than 10$</li>
+        <li>Greater than 10$</li>
+        <li>Popular</li>
+        <li>Not popular</li>
+        <li>Show all</li>
+        </ul>
+    </div>
+  )
+}
--- src/pages/Psychologists/PsychologistsPage.jsx
+++ src/pages/Psychologists/PsychologistsPage.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { PsychologistsList } from "../../components/PsychologistsList/PsychologistsList";
+import { Filter } from "../../components/Filter/Filter";
 
 export const PsychologistsPage = () => {
   return <div>
+    <Filter/>
     <PsychologistsList/>
   </div>;
 };
