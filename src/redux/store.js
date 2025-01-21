536d87a68fd81877634894f9bca04298d669095e
Natali258 <natalya.holyavko@gmail.com>
2025-01-21 16:19:50 +0200

    hidden store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,50 +1,50 @@
-import { configureStore } from '@reduxjs/toolkit';
-import storage from 'redux-persist/lib/storage';
-import {
-  persistStore,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist';
-import persistReducer from 'redux-persist/es/persistReducer';
-import { authReducer } from './Auth/AuthSlice';
-import { psychologistsReducer } from './Psychologists/PsychologistsSlice';
-import { filterValueReducer } from './Filter/FilterSlice';
+// import { configureStore } from '@reduxjs/toolkit';
+// import storage from 'redux-persist/lib/storage';
+// import {
+//   persistStore,
+//   FLUSH,
+//   REHYDRATE,
+//   PAUSE,
+//   PERSIST,
+//   PURGE,
+//   REGISTER,
+// } from 'redux-persist';
+// import persistReducer from 'redux-persist/es/persistReducer';
+// import { authReducer } from './Auth/AuthSlice';
+// import { psychologistsReducer } from './Psychologists/PsychologistsSlice';
+// import { filterValueReducer } from './Filter/FilterSlice';
 
-const persistConfig = {
-  key: 'userItem',
-  storage,
-  whitelist: ['token'],
-};
-const persistedReducer = persistReducer(persistConfig, authReducer);
-
-// const peConfig = {
-//   key: 'psychologistsItem',
+// const persistConfig = {
+//   key: 'userItem',
 //   storage,
+//   whitelist: ['token'],
 // };
-// const pedReducer = persistReducer(peConfig, psychologistsReducer);
+// const persistedReducer = persistReducer(persistConfig, authReducer);
 
-// const filterConfig = {
-//   key: 'filterItem',
-//   storage
-// };
-// const filterReducer = persistReducer(filterConfig, filterValueReducer)
+// // const peConfig = {
+// //   key: 'psychologistsItem',
+// //   storage,
+// // };
+// // const pedReducer = persistReducer(peConfig, psychologistsReducer);
 
-export const store = configureStore({
-  reducer: {
-    auth: persistedReducer,
-    // psychologist: pedReducer,
-    // filter: filterReducer,
-  },
-  middleware: getDefaultMiddleware =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
-});
+// // const filterConfig = {
+// //   key: 'filterItem',
+// //   storage
+// // };
+// // const filterReducer = persistReducer(filterConfig, filterValueReducer)
 
-export let persistor = persistStore(store);
+// export const store = configureStore({
+//   reducer: {
+//     auth: persistedReducer,
+//     // psychologist: pedReducer,
+//     // filter: filterReducer,
+//   },
+//   middleware: getDefaultMiddleware =>
+//     getDefaultMiddleware({
+//       serializableCheck: {
+//         ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+//       },
+//     }),
+// });
+
+// export let persistor = persistStore(store);
