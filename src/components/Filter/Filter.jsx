0a92d8c6204ff51eb9df990471b4dba68d8e625a
Natali258 <natalya.holyavko@gmail.com>
2025-04-23 18:00:42 +0300

    fix filter styled select

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from 'react'
 import {FilterContainer, FilterTitle} from './Filter.styled'
-import { colors, MenuItem, OutlinedInput, outlinedInputClasses, Select, selectClasses} from '@mui/material'
+import { colors, Menu, MenuItem, OutlinedInput, outlinedInputClasses, Select, selectClasses} from '@mui/material'
 import { styled } from '@mui/material/styles';
 import { BorderColor, Padding } from '@mui/icons-material';
+import shadows from '@mui/material/styles/shadows';
 
 export const Filter = ({setFilter}) => {
     const [filter, setFilterLocal] = useState('atoz')
@@ -19,19 +20,16 @@ export const Filter = ({setFilter}) => {
         borderRadius: '14px',
         border: 'none',
         '& .MuiOutlinedInput-input': {padding: '14px 0 14px 18px', width: '162px'},
-        '& .MuiOutlinedInput-notchedOutline': {border: 'none'},
-        
-        // "&.Mui-focused fieldset": { borderColor: "red" },
-        // '& .MuiInputBase-input': {outline: 'none'},
-        // padding: '16px 162px 16px 18px',
-        // border: 'none',
-        // '& .MuiSelect-icon': {
-        //   color: '#666',
-        // },
-        // '&.Mui-focused .MuiOutlinedInput-notchedOutline': {
-        //   borderColor: theme.palette.primary.main,
-        // },
+        '& .MuiOutlinedInput-notchedOutline': {border: 'none'}
       });
+      const SFilterMenuItem = styled(MenuItem)({
+        backgroundColor: 'none',
+        color: 'rgba(25, 26, 21, 0.3)',
+        '&:hover': {backgroundColor: '#54be96'},
+        '&.Mui-focusVisible': {backgroundColor: '#54be96'},
+        '&.Mui-selected': {backgroundColor: '#54be96', сolor: '#191a15'}
+      });
+
       
       // const handleResetFilter = (e) => {
       //   setFilterLocal('A to Z');
@@ -45,14 +43,24 @@ export const Filter = ({setFilter}) => {
       <FilterTitle>Filters</FilterTitle>
       <SFilterSelect
         value={filter}
+        MenuProps={{
+          PaperProps: {
+            sx: {
+              borderRadius: '14px',
+              boxShadow: '0 20px 69px 0 rgba(0, 0, 0, 0.07)',
+              backgroundColor: '#fff',
+              mt: '8px'
+            },
+          },
+        }}
       >
-          <MenuItem onClick={()=>handleFilter('atoz')} value='atoz' data-key="byABC">A to Z</MenuItem>
-          <MenuItem onClick={()=>handleFilter('ztoa')} value='ztoa' data-key="byABC">Z to A</MenuItem>
-          <MenuItem onClick={()=>handleFilter('less10')} value='less10' data-key="byPrice">Less than 10$</MenuItem>
-          <MenuItem onClick={()=>handleFilter('greater10')} value='greater10' data-key="byPrice">Greater than 10$</MenuItem>
-          <MenuItem onClick={()=>handleFilter('popular')} value='popular' data-key="byPopularity">Popular</MenuItem>
-          <MenuItem onClick={()=>handleFilter('notpopular')} value='notpopular' data-key="byPopularity">Not popular</MenuItem>
-          <MenuItem onClick={()=>handleFilter('showAll')} value='showAll' data-key="showAll">Show all</MenuItem>
+          <SFilterMenuItem onClick={()=>handleFilter('atoz')} value='atoz' data-key="byABC">A to Z</SFilterMenuItem>
+          <SFilterMenuItem onClick={()=>handleFilter('ztoa')} value='ztoa' data-key="byABC">Z to A</SFilterMenuItem>
+          <SFilterMenuItem onClick={()=>handleFilter('less10')} value='less10' data-key="byPrice">Less than 10$</SFilterMenuItem>
+          <SFilterMenuItem onClick={()=>handleFilter('greater10')} value='greater10' data-key="byPrice">Greater than 10$</SFilterMenuItem>
+          <SFilterMenuItem onClick={()=>handleFilter('popular')} value='popular' data-key="byPopularity">Popular</SFilterMenuItem>
+          <SFilterMenuItem onClick={()=>handleFilter('notpopular')} value='notpopular' data-key="byPopularity">Not popular</SFilterMenuItem>
+          <SFilterMenuItem onClick={()=>handleFilter('showAll')} value='showAll' data-key="showAll">Show all</SFilterMenuItem>
       </SFilterSelect>
     </FilterContainer>
   )
