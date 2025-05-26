52e4d8be82cbbd57716b311f1b72ab3c68d176f6
Natali258 <natalya.holyavko@gmail.com>
2025-05-26 16:46:15 +0300

    add Loyout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import { Header } from './components/Header/Header';
+// import { Header } from './components/Header/Header';
 import { HomePage } from './pages/HomePage/HomePage';
 import { PsychologistsPage } from './pages/Psychologists/PsychologistsPage';
 import { FavoritesPage } from './pages/Favorites/FavoritesPage';
+import { Loyout } from './components/Loyout';
 
 
 function App() {
 
 
   return (
-    <div>
-      <Header/>
+    <>
       <Routes>
-        <Route path='/' element={<HomePage/>}/>
-        <Route path='/psychologists' element={<PsychologistsPage/>}/>
-        <Route path='/favorites' element={<FavoritesPage/>}/>
+        <Route path='/' element={<Loyout/>}>
+          <Route path='/' element={<HomePage/>}/>
+          <Route path='/psychologists' element={<PsychologistsPage/>}/>
+          <Route path='/favorites' element={<FavoritesPage/>}/>
+        </Route>
       </Routes>
-    </div>
+    </>
   )
 }
 
