c99372723357e910d94e43da4a1a563fe9458de8
Natali258 <natalya.holyavko@gmail.com>
2025-05-26 22:49:57 +0300

    used hook useModal

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
-// import { Header } from './components/Header/Header';
 import { HomePage } from './pages/HomePage/HomePage';
 import { PsychologistsPage } from './pages/Psychologists/PsychologistsPage';
 import { FavoritesPage } from './pages/Favorites/FavoritesPage';
@@ -15,9 +14,9 @@ function App() {
     <>
       <Routes>
         <Route path='/' element={<Loyout/>}>
-          <Route path='/' element={<HomePage/>}/>
-          <Route path='/psychologists' element={<PsychologistsPage/>}/>
-          <Route path='/favorites' element={<FavoritesPage/>}/>
+          <Route index element={<HomePage/>}/>
+          <Route path='psychologists' element={<PsychologistsPage/>}/>
+          <Route path='favorites' element={<FavoritesPage/>}/>
         </Route>
       </Routes>
     </>
--- src/components/AuthorizationMenu/AuthorizationMenu.jsx
+++ src/components/AuthorizationMenu/AuthorizationMenu.jsx
@@ -2,30 +2,24 @@ import React from 'react'
 import { LoginForm } from '../LoginForm/LoginForm';
 import { RegisterForm } from '../RegisterForm/RegisterForm';
 import { SHeaderBtn, SHeaderBtnRegistration, SHeaderUlBtn } from './AuthorizationMenu.styled';
+import { useModal } from '../hooks/useModal';
 
 export const AuthorizationMenu = () => {
-    const [openLog, setOpenLog] = React.useState(false);
-        const handleOpenLog = () => setOpenLog(true);
-        const handleCloseLog = () => setOpenLog(false);
-    
-        const [open, setOpen] = React.useState(false);
-        const handleOpen = () => setOpen(true);
-        const handleClose = () => setOpen(false);
-
+  const [isOpenModal, toggleModal] = useModal();
 
 
   return (
     <div>
         <SHeaderUlBtn>
                     <li>
-                        <SHeaderBtn onClick={handleOpenLog}>Log In</SHeaderBtn>
+                        <SHeaderBtn onClick={toggleModal}>Log In</SHeaderBtn>
                     </li>
-                    <li><SHeaderBtnRegistration onClick={handleOpen}>Registration</SHeaderBtnRegistration></li>
+                    <li><SHeaderBtnRegistration onClick={toggleModal}>Registration</SHeaderBtnRegistration></li>
         </SHeaderUlBtn>
-                <LoginForm open={openLog}
-                onClose={handleCloseLog}/>
-                <RegisterForm open={open}
-                onClose={handleClose}/>
+                <LoginForm open={isOpenModal}
+                onClose={toggleModal}/>
+                <RegisterForm open={isOpenModal}
+                onClose={toggleModal}/>
     </div>
   )
 }
