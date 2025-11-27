d3baeb7de32aa784ecab7ca7b0071de109f100e2
Natali258 <natalya.holyavko@gmail.com>
2025-11-27 14:59:54 +0200

    add LogOut

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -1,9 +1,19 @@
 
+import { signOut } from 'firebase/auth';
 import { IconSvg } from '../Icon/IconSvg';
 import { SUserBox, SUserBoxAvatar, SUserBoxName, SUserBtn, SUserContainer } from './UserMenu.styled'
+import { auth } from '../../services/FirebaseApp';
 
 
 export const UserMenu = ({userName}) => {
+  const handleExit = async() => {
+    try {
+    await signOut(auth);
+    console.log("User signed out");
+  } catch (error) {
+    console.error("Error signing out:", error);
+  }
+  }
   return (
     <SUserContainer>
         <SUserBox>
@@ -12,7 +22,7 @@ export const UserMenu = ({userName}) => {
           </SUserBoxAvatar>
           <SUserBoxName>{userName}</SUserBoxName>
         </SUserBox>
-        <SUserBtn>Log out</SUserBtn>
+        <SUserBtn onClick={handleExit}>Log out</SUserBtn>
     </SUserContainer>
   )
 }
